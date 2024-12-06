da63ef417819d320217373a2b46f912733276fe1
heywowyou <180994236+heywowyou@users.noreply.github.com>
2024-12-06 21:43:57 +0100

    Add user inputed number into the create grid function

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const gridContainer = document.querySelector("#grid-container");
 
-for (let i = 0; i < 4; i++) {
-  let divContainer = document.createElement("div");
-  divContainer.classList.add("div-container");
-  gridContainer.appendChild(divContainer);
+let userInput;
 
-  for (let j = 0; j < 4; j++) {
-    let div = document.createElement("div");
-    div.classList.add("div");
-    divContainer.appendChild(div);
-    div.addEventListener("mouseover", () => {
-      div.classList.add("div-colored");
-    });
+function createGrid() {
+  for (let i = 0; i < userInput; i++) {
+    let divContainer = document.createElement("div");
+    divContainer.classList.add("div-container");
+    gridContainer.appendChild(divContainer);
+
+    for (let j = 0; j < userInput; j++) {
+      let div = document.createElement("div");
+      div.classList.add("div");
+      divContainer.appendChild(div);
+      div.addEventListener("mouseover", () => {
+        div.classList.add("div-colored");
+      });
+    }
   }
 }
 
 const button = document.querySelector("#button");
 
 button.addEventListener("click", () => {
-  const userInput = prompt("Please select grid size");
+  userInput = prompt("Please select grid size");
+  Number(userInput);
+  createGrid();
 });
